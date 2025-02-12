30afb264a1bba6a110fb50bb530f99bdabde1c3b
devkavin <kavindra.senanayake@gmail.com>
2025-02-12 16:40:22 +0530

    migration files created for watchlist, movie, and tv_series

---
+++ database/migrations/2025_02_11_185305_create_tv_series_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tv_series', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image_url')->nullable();
+            $table->string('description')->nullable();
+            $table->string('ep_count')->nullable();
+            $table->string('watch_url')->nullable();
+            $table->string('imdb_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tv_series');
+    }
+};
